59d26ae32319fc1527a70105b73e0186b4ffd15b
DevJtaiwo <taiwoayomide202@gmail.com>
2024-08-10 10:24:19 +0100

    talk app

---
+++ speak.py
@@ -0,0 +1,9 @@
+import pyttsx3
+
+akanni = pyttsx3.init()
+
+akanni.say("Hi, welcome to Lord BillJay Holdings, what will you like to buy from us? There are varieties of goods to choose from, ranging from biscuit, bread, table water, milks and family water.")
+
+akanni.say("I am waiting for your response")
+
+akanni.runAndWait()
